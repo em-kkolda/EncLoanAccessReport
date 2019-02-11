343db18095b48b3966473839011d026af3fbecf0
Ken Kolda <Ken.Kolda@elliemae.com>
2019-02-11 09:16:16 -0800

    Initial check-in

---
+++ Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.IO;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using EllieMae.Encompass.Client;
+using EllieMae.Encompass.BusinessObjects.Users;
+using EllieMae.Encompass.Collections;
+using EllieMae.Encompass.Reporting;
+
+namespace EncLoanAccessReport
+{
+    class Program
+    {
+        // Constants for the program return code
+        private const int ResultSuccess = 0;
+        private const int ResultFailure = 1;
+
+        // Command-line arguments
+        private static string serverUri = null;
+        private static string userId = null;
+        private static string password = null;
+        private static string outputFilePath = null;
+
+        static int Main(string[] args)
+        {
+            try
+            {
+                // Parse the command line
+                if (!readCommandLineArguments(args))
+                    return ResultFailure;
+
+                // Log into the Encompass Server
+                using (Session session = authenticate())
+                {
+                    if (session == null)
+                        return ResultFailure;
+
+                    // Generate the user loan access information
+                    var userLoanCounts = generateLoanAccessInformation(session);
+
+                    // Dump the output to stdout
+                    using (TextWriter output = openOutputStream())
+                        writeReport(userLoanCounts, output);
+                }
+
+                return ResultSuccess;
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("Unexpected error: " + ex);
+                return ResultFailure;
+            }
+        }
+
+        // Read the command-line parameters
+        static bool readCommandLineArguments(string[] args)
+        {
+            for (int i = 1; i < args.Length; i += 2)
+            {
+                switch (args[i - 1].ToLower())
+                {
+                    case "-u": userId = args[i]; break;
+                    case "-p": password = args[i]; break;
+                    case "-s": serverUri = args[i]; break;
+                    case "-o": outputFilePath = args[i]; break;
+                    default:
+                        Console.Error.WriteLine("Unknown command line argument '{0}'", args[i - 1]);
+                        return false;
+                }
+            }
+
+            // Verify if minimum parameters were provided
+            if (serverUri == null)
+            {
+                Console.Error.WriteLine("Missing required command-line parameter: -s <serverUri>");
+                return false;
+            }
+
+            if (userId == null)
+            {
+                Console.Error.WriteLine("Missing required command-line parameter: -u <userId>");
+                return false;
+            }
+
+            if (password == null)
+            {
+                Console.Error.WriteLine("Missing required command-line parameter: -p <password>");
+                return false;
+            }
+
+            return true;
+        }
+
+        // Logs into the Encompass Server
+        static Session authenticate()
+        {
+            try
+            {
+                Session session = new Session();
+                session.Start(serverUri, userId, password);
+                return session;
+            }
+            catch (LoginException ex)
+            {
+                Console.Error.WriteLine("Login Failed for reason: " + ex.ErrorType);
+                return null;
+            }
+        }
+
+        // Generates the number of loans accessible by each Encompass user
+        static Dictionary<string, int> generateLoanAccessInformation(Session session)
+        {
+            // Create the result set dictionary
+            Dictionary<string, int> userLoanAccessCounts = new Dictionary<string, int>();
+
+            // Get the list of all users from the server
+            UserList users = session.Users.GetAllUsers();
+
+            foreach (User user in users)
+            {
+                try
+                {
+                    // Impersonate this user
+                    session.ImpersonateUser(user.ID);
+
+                    // Retrieve a loan cursor with all loans (i.e. no filter applied). We pass an empty
+                    // field list since we don't need any actual field data.
+                    StringList fields = new StringList();
+
+                    using (LoanReportCursor cursor = session.Reports.OpenReportCursor(fields, null))
+                    {
+                        // The number of loans in the cursor is the total number this user can access
+                        userLoanAccessCounts[user.ID] = cursor.Count;
+
+                        // Show a tick on the console
+                        Console.Error.Write(".");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine("Failed to retrieve loan count for user {0}: {1}", user.ID, ex);
+                }
+                finally
+                {
+                    // End the impersonation
+                    session.RestoreIdentity();
+                }
+            }
+
+            // Write an endline to the error console
+            Console.Error.WriteLine();
+
+            return userLoanAccessCounts;
+        }
+
+        static TextWriter openOutputStream()
+        {
+            if (outputFilePath == null)
+                return Console.Out;
+            else
+                return new StreamWriter(outputFilePath, false);
+        }
+
+        // Writes the results to an output strean
+        static void writeReport(Dictionary<string, int> userLoanCounts, TextWriter outputStream)
+        {
+            userLoanCounts.ToList().ForEach(item => outputStream.WriteLine("{0}\t{1}", item.Key, item.Value));
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+﻿# Encompass Loan Access Report Tool
+This tool produces a report of all Encompass users in a system and the number of Loans that the user can access (read). The tool is meant to 
+provide system and security administrators of an Encompass system with a means to audit their users' access. Run periodically, it can help
+your organization avoid "access creep" and adhere to a least privileges approach to data access.
+
+## Command-line Options
+Once compiled, you can run the tool on the command line as follows:
+
+```
+PROMPT> encloanaccessreport -u <sa_uid> -p <sa_pwd> -s <server_url> -o <output_file>
+```
+
+The `sa_uid` and `sa_pwd` parameters are the credentials for an Encompass user with Super Administrator privileges. A highly privileged
+account is required by this tool in order to "impersonate" other users and determine their level of visibility.
+
+The `server_url` is the URL of yoru Encompass Server, which typically has the format `https://<iid>.ea.elliemae.net$<iid>`, where
+`scid` is your Encompass Instance ID (e.g. "BE11112222").
+
+The `output_file` parameter is optional and can be used to direct the output to a file. Otherwise, the output will be written to
+stdout (i.e. the console window).
+
+## SDK Licensing
+This tool leverages the Encompass SDK, which is an integration tool available to you as part of your Encompass license. However,
+using the SDK requires that the machine on which the program will run be registered using an SDK Key generated for your organization.
+If you do not already have an SDK Key, contact your Account Represetative for more information.
